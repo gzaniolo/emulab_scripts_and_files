93d9f70aa1fdcc87a325c1b73110d87552e03725
Giancarlo Zaniolo <gzaniolo@andrew.cmu.edu>
2024-12-13 09:49:29 -0500

    small changes

--- setup_db.sh
+++ setup_db.sh
@@ -5,14 +5,16 @@ sudo systemctl enable postgresql  # This will ensure PostgreSQL starts on boot
 sudo systemctl status postgresql
 
 
-sudo -i -u postgres psql -c "CREATE USER admin WITH PASSWORD 'password';"
-sudo -i -u postgres psql -c "CREATE DATABASE benchbase;"
-sudo -i -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE benchbase TO admin;"
-
 # TODO change the files
 sudo cp $HOME/emulab_scripts_and_files/postgresql.conf /etc/postgresql/14/main/postgresql.conf 
 
 sudo cp $HOME/emulab_scripts_and_files/pg_hba.conf /etc/postgresql/14/main/pg_hba.conf 
+
+sudo -i -u postgres psql -c "CREATE USER admin WITH PASSWORD 'password';"
+sudo -i -u postgres psql -c "CREATE DATABASE benchbase;"
+sudo -i -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE benchbase TO admin;"
+
+sudo systemctl reload postgresql
 
 echo "WARNING WARNING WARNING WARNING WARNING WARNING WARNING"
 echo "If database is not working, exit, log back in, and 'systemctl restart postgresql'"
